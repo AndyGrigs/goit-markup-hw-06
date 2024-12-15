2abea471916e9c88dffdc5dc7ba3a4c8eb3e389d
Andriy <andygrigs88@gmail.com>
2024-12-15 12:51:02 +0100

    script to add

--- index.js
+++ index.js
@@ -19,37 +19,3 @@ document.addEventListener("click", (e) => {
 });
 
 
-
-// Select elements
-const modalContainer = document.getElementById("modal");
-const modalCloseButton = document.getElementById("modal-close");
-const modalOpenLinks = document.querySelectorAll("[data-modal-open]");
-
-// Function to open the modal
-function openModal() {
-  modalContainer.classList.add("open");
-}
-
-// Function to close the modal
-function closeModal() {
-  modalContainer.classList.remove("open");
-}
-
-// Open modal when clicking links with 'data-modal-open'
-modalOpenLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    openModal();
-  });
-});
-
-// Close modal when clicking the close button
-modalCloseButton.addEventListener("click", closeModal);
-
-// Close modal when clicking outside the modal content
-modalContainer.addEventListener("click", (e) => {
-  if (e.target === modalContainer) {
-    closeModal();
-  }
-});
-
